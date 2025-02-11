25b7564eb68115a8d868f1cbb85b3716d9cc206b
halfanvicent <hassani.fundi@dart.go.tz>
2025-02-11 10:39:36 +0300

    Create ticket

--- ticket-processing-system/src/processor/processor.hpp
+++ ticket-processing-system/src/processor/processor.hpp
@@ -1,14 +1,22 @@
 #ifndef PROCESSOR_HPP
 #define PROCESSOR_HPP
 
+#include <vector>
 #include "../ticket/ticket.hpp"
 #include "../validators/input_validator.hpp"
 
 class TicketProcessor {
+
+private:
+    std::vector<Ticket> tickets;
+
 public:
     TicketProcessor();
 
-    Ticket createTicket(const std::string& fareInput);
+    void TicketProcessor::createTicket(int id, double fare, const std::string& product, char route) {
+        Ticket newTicket(id, fare, product, route);
+        tickets.push_back(newTicket);
+    }
     void processTicket(Ticket& ticket);
     void applyDiscount(Ticket& ticket, double discountPercentage);
 };
